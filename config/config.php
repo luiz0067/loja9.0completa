29c1dd57427870ab60a2bcb532caaa747a22ecb1
Luiz Fernando Brogliatto Ferreira <luiz0067@gmail.com>
2021-10-09 15:43:00 -0300

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,336 @@
+<?php
+
+	// Last Updated: 24th Apr 2010 @ 11:40 AM
+
+	$GLOBALS['ISC_CFG']["isSetup"] = false;
+	$GLOBALS['ISC_CFG']["Language"] = 'br';
+	$GLOBALS['ISC_CFG']["AllowPurchasing"] = "1";
+	$GLOBALS['ISC_CFG']["serverStamp"] = "";
+	$GLOBALS['ISC_CFG']["HostingProvider"] = "";
+	$GLOBALS['ISC_CFG']["UseWYSIWYG"] = 1;
+	$GLOBALS['ISC_CFG']["dbType"] = "mysql";
+	$GLOBALS['ISC_CFG']["dbEncoding"] = "latin1";
+	$GLOBALS['ISC_CFG']["dbServer"] = "";
+	$GLOBALS['ISC_CFG']["dbUser"] = "";
+	$GLOBALS['ISC_CFG']["dbPass"] = "";
+	$GLOBALS['ISC_CFG']["dbDatabase"] = "";
+	$GLOBALS['ISC_CFG']["tablePrefix"] = "isc_";
+	$GLOBALS['ISC_CFG']["StoreName"] = 'WebShop - Lojas virtuais';
+	$GLOBALS['ISC_CFG']["StoreAddress"] = 'BR/BR';
+	$GLOBALS['ISC_CFG']["LogoType"] = 'text';
+	$GLOBALS['ISC_CFG']["StoreLogo"] = '';
+	$GLOBALS['ISC_CFG']["ShopPath"] = "http://";
+	$GLOBALS['ISC_CFG']["CharacterSet"] = 'ISO-8859-1';
+	$GLOBALS['ISC_CFG']["HomePagePageTitle"] = '';
+	$GLOBALS['ISC_CFG']["MetaKeywords"] = '';
+	$GLOBALS['ISC_CFG']["MetaDesc"] = '';
+	$GLOBALS['ISC_CFG']["DownloadDirectory"] = 'product_downloads';
+	$GLOBALS['ISC_CFG']["ImageDirectory"] = 'product_images';
+	$GLOBALS['ISC_CFG']["template"] = 'laranja';
+	$GLOBALS['ISC_CFG']["SiteColor"] = 'blue';
+	$GLOBALS['ISC_CFG']["CurrencyToken"] = ' R$ ';
+	$GLOBALS['ISC_CFG']["CurrencyLocation"] = 'left';
+	$GLOBALS['ISC_CFG']["DecimalToken"] = ',';
+	$GLOBALS['ISC_CFG']["DecimalPlaces"] = 2;
+	$GLOBALS['ISC_CFG']["ThousandsToken"] = '.';
+	$GLOBALS['ISC_CFG']["InstallDate"] = 1272077455;
+
+	// SSL Settings
+	$GLOBALS['ISC_CFG']["UseSSL"] = '0';
+	$GLOBALS['ISC_CFG']["SharedSSLPath"] = '';
+	$GLOBALS['ISC_CFG']["SubdomainSSLPath"] = '';
+	$GLOBALS['ISC_CFG']["ForceControlPanelSSL"] = 0;
+
+	// Physical Dimensions Settings
+	$GLOBALS['ISC_CFG']["WeightMeasurement"] = 'Grams';
+	$GLOBALS['ISC_CFG']["LengthMeasurement"] = 'Centimeters';
+	$GLOBALS['ISC_CFG']["DimensionsDecimalToken"] = ',';
+	$GLOBALS['ISC_CFG']["DimensionsDecimalPlaces"] = '0';
+	$GLOBALS['ISC_CFG']["DimensionsThousandsToken"] = '.';
+
+	$GLOBALS['ISC_CFG']["DisplayDateFormat"] = 'jS M Y';
+	$GLOBALS['ISC_CFG']["ExportDateFormat"] = 'jS M Y';
+	$GLOBALS['ISC_CFG']["ExtendedDisplayDateFormat"] = 'jS M Y @ g:i A';
+	$GLOBALS['ISC_CFG']["HomeFeaturedProducts"] = 4;
+	$GLOBALS['ISC_CFG']["HomeNewProducts"] = 16;
+	$GLOBALS['ISC_CFG']["HomeBlogPosts"] = 10;
+	$GLOBALS['ISC_CFG']["CategoryProductsPerPage"] = 16;
+	$GLOBALS['ISC_CFG']["CategoryListDepth"] = 2;
+	$GLOBALS['ISC_CFG']["ProductReviewsPerPage"] = 10;
+	$GLOBALS['ISC_CFG']["TagCloudsEnabled"] = 1;
+	$GLOBALS['ISC_CFG']["ShowAddToCartQtyBox"] = 1;
+	$GLOBALS['ISC_CFG']["CaptchaEnabled"] = 1;
+	$GLOBALS['ISC_CFG']["ShowCartSuggestions"] = 1;
+	$GLOBALS['ISC_CFG']["AdminEmail"] = "";
+	$GLOBALS['ISC_CFG']["OrderEmail"] = "";
+	$GLOBALS['ISC_CFG']['LowInventoryNotificationAddress'] = '';
+	$GLOBALS['ISC_CFG']["ShowThumbsInCart"] = 1;
+	$GLOBALS['ISC_CFG']["AutoApproveReviews"] = 0;
+	$GLOBALS['ISC_CFG']["SearchSuggest"] = 1;
+	$GLOBALS['ISC_CFG']["QuickSearch"] = 1;
+	$GLOBALS['ISC_CFG']["TaxTypeSelected"] = 0;
+	$GLOBALS['ISC_CFG']["PricesIncludeTax"] = 1;
+	$GLOBALS['ISC_CFG']["DefaultTaxRateName"] = '';
+	$GLOBALS['ISC_CFG']["DefaultTaxRate"] = 0;
+	$GLOBALS['ISC_CFG']["DefaultTaxRateBasedOn"] = 'subtotal';
+
+	// Shipping Settings
+	$GLOBALS['ISC_CFG']["CompanyName"] = 'Webshop - Lojas virtuais';
+	$GLOBALS['ISC_CFG']["CompanyAddress"] = 'www.cliquemania.com';
+	$GLOBALS['ISC_CFG']["CompanyCity"] = 'SÃO VICENTE';
+	$GLOBALS['ISC_CFG']["CompanyCountry"] = 'Brasil';
+	$GLOBALS['ISC_CFG']["CompanyState"] = 'São Paulo';
+	$GLOBALS['ISC_CFG']["CompanyZip"] = '11355450';
+
+	// Checkout Settings
+	$GLOBALS['ISC_CFG']["CheckoutMethods"] = 'checkout_digital';
+	$GLOBALS['ISC_CFG']['CheckoutType'] = 'single';
+	$GLOBALS['ISC_CFG']['GuestCheckoutEnabled'] = 1;
+	$GLOBALS['ISC_CFG']['GuestCheckoutCreateAccounts'] = 1;
+
+	$GLOBALS['ISC_CFG']["ShowThumbsInControlPanel"] = 1;
+	$GLOBALS['ISC_CFG']["EnableSEOUrls"] = 1;
+	$GLOBALS['ISC_CFG']['ShowInventory'] = 0;
+	$GLOBALS['ISC_CFG']['StoreTimeZone'] = '-3';
+	$GLOBALS['ISC_CFG']['StoreDSTCorrection'] = 0;
+	$GLOBALS['ISC_CFG']['ShowDownloadTemplates'] = '1';
+	$GLOBALS['ISC_CFG']['TagCartQuantityBoxes'] = 'dropdown';
+
+	$GLOBALS['ISC_CFG']["RSSNewProducts"] = 1;
+	$GLOBALS['ISC_CFG']["RSSPopularProducts"] = 1;
+	$GLOBALS['ISC_CFG']["RSSCategories"] = 1;
+	$GLOBALS['ISC_CFG']["RSSProductSearches"] = 1;
+	$GLOBALS['ISC_CFG']["RSSLatestBlogEntries"] = 1;
+	$GLOBALS['ISC_CFG']["RSSItemsLimit"] = 10;
+	$GLOBALS['ISC_CFG']["RSSCacheTime"] = 60;
+	$GLOBALS['ISC_CFG']["RSSSyndicationIcons"] = 1;
+
+	$GLOBALS['ISC_CFG']['BackupsLocal'] = 1;
+	$GLOBALS['ISC_CFG']['BackupsRemoteFTP'] = 0;
+	$GLOBALS['ISC_CFG']['BackupsRemoteFTPHost'] = '';
+	$GLOBALS['ISC_CFG']['BackupsRemoteFTPUser'] = '';
+	$GLOBALS['ISC_CFG']['BackupsRemoteFTPPass'] = '';
+	$GLOBALS['ISC_CFG']['BackupsRemoteFTPPath'] = '';
+	$GLOBALS['ISC_CFG']['BackupsAutomatic'] = 0;
+	$GLOBALS['ISC_CFG']['BackupsAutomaticMethod'] = 'local';
+	$GLOBALS['ISC_CFG']['BackupsAutomaticDatabase'] = 1;
+	$GLOBALS['ISC_CFG']['BackupsAutomaticImages'] = 1;
+	$GLOBALS['ISC_CFG']['BackupsAutomaticDownloads'] = 1;
+
+	$GLOBALS['ISC_CFG']["GoogleMapsAPIKey"] = '';
+	$GLOBALS['ISC_CFG']["NotificationMethods"] = '';
+	$GLOBALS['ISC_CFG']["CurrencyMethods"] = 'currency_webservicex';
+	$GLOBALS['ISC_CFG']["DefaultCurrencyID"] = 1;
+	$GLOBALS['ISC_CFG']["DefaultCurrencyRate"] = 1;
+
+	$GLOBALS['ISC_CFG']["MailXMLAPIValid"] = 0;
+	$GLOBALS['ISC_CFG']["MailXMLPath"] = '';
+	$GLOBALS['ISC_CFG']["MailXMLToken"] = '';
+	$GLOBALS['ISC_CFG']["MailUsername"] = '';
+	$GLOBALS['ISC_CFG']["UseMailerForNewsletter"] = 0;
+	$GLOBALS['ISC_CFG']["UseMailerForOrders"] = 0;
+	$GLOBALS['ISC_CFG']["MailNewsletterList"] = 0;
+	$GLOBALS['ISC_CFG']["MailNewsletterFormat"] = 'h';
+	$GLOBALS['ISC_CFG']["MailNewsletterCustomField"] = 0;
+	$GLOBALS['ISC_CFG']["MailOrderList"] = 0;
+	$GLOBALS['ISC_CFG']["MailOrderFormat"] = 'h';
+	$GLOBALS['ISC_CFG']["MailOrderListAutoSubscribe"] = 0;
+	$GLOBALS['ISC_CFG']["MailOrderFirstName"] = 0;
+	$GLOBALS['ISC_CFG']["MailOrderLastName"] = 0;
+	$GLOBALS['ISC_CFG']["MailOrderFullName"] = 0;
+	$GLOBALS['ISC_CFG']["MailOrderZip"] = 0;
+	$GLOBALS['ISC_CFG']["MailOrderCountry"] = 0;
+	$GLOBALS['ISC_CFG']["MailOrderTotal"] = 0;
+	$GLOBALS['ISC_CFG']["MailOrderPaymentMethod"] = 0;
+	$GLOBALS['ISC_CFG']["MailOrderShippingMethod"] = 0;
+	$GLOBALS['ISC_CFG']["MailAutomaticallyTickNewsletterBox"] = 0;
+	$GLOBALS['ISC_CFG']["MailAutomaticallyTickOrderBox"] = 0;
+	$GLOBALS['ISC_CFG']["UseMailAPIForUpdates"] = 0;
+	$GLOBALS['ISC_CFG']["MailProductUpdatesListType"] = '';
+	$GLOBALS['ISC_CFG']["MailProductUpdatesFormat"] = 'h';
+	$GLOBALS['ISC_CFG']['ShowMailingListInvite'] = 1;
+
+	$GLOBALS['ISC_CFG']["AnalyticsMethods"] = '';
+
+	$GLOBALS['ISC_CFG']['SystemLogging'] = 1;
+	$GLOBALS['ISC_CFG']['HidePHPErrors'] = 0;
+	$GLOBALS['ISC_CFG']['SystemLogTypes'] = 'general,payment,shipping,notification,ssnx,sql,php,accounting';
+	$GLOBALS['ISC_CFG']['SystemLogSeverity'] = 'errors,warnings,success,notices';
+	$GLOBALS['ISC_CFG']['SystemLogMaxLength'] = 0;
+	$GLOBALS['ISC_CFG']['AdministratorLogging'] = 1;
+	$GLOBALS['ISC_CFG']['AdministratorLogMaxLength'] = 0;
+	$GLOBALS['ISC_CFG']['DebugMode'] = 0;
+
+	$GLOBALS['ISC_CFG']['EnableReturns'] = 1;
+	$GLOBALS['ISC_CFG']['ReturnReasons'] = array (
+  0 => 'Received Wrong Product',
+  1 => 'Wrong Product Ordered',
+  2 => 'Not Satisfied With The Product',
+  3 => 'There Was A Problem With The Product',
+);
+	$GLOBALS['ISC_CFG']['ReturnActions'] = array (
+  0 => 'Repair',
+  1 => 'Replacement',
+  2 => 'Store Credit',
+);
+	$GLOBALS['ISC_CFG']['ReturnCredits'] = 1;
+	$GLOBALS['ISC_CFG']['ReturnInstructions'] = '';
+	$GLOBALS['ISC_CFG']['EmailOwnerOnReturn'] = 1;
+	$GLOBALS['ISC_CFG']['SendReturnConfirmation'] = 1;
+	$GLOBALS['ISC_CFG']['NotifyOnReturnStatusChange'] = 1;
+
+	$GLOBALS['ISC_CFG']['EnableGiftCertificates'] = 1;
+	$GLOBALS['ISC_CFG']['GiftCertificateAmounts'] = array (
+);
+	$GLOBALS['ISC_CFG']['GiftCertificateCustomAmounts'] = 1;
+	$GLOBALS['ISC_CFG']['GiftCertificateMinimum'] = 1;
+	$GLOBALS['ISC_CFG']['GiftCertificateMaximum'] = 1000;
+	$GLOBALS['ISC_CFG']['GiftCertificateExpiry'] = 0;
+	$GLOBALS['ISC_CFG']['GiftCertificateThemes'] = 'Birthday.html,Boy.html,Celebration.html,Christmas.html,General.html,Girl.html';
+
+	$GLOBALS['ISC_CFG']['UpdateInventoryLevels'] = 1;
+	$GLOBALS['ISC_CFG']['OrderStatusNotifications'] = '1,7,11,9,8,3,10,2,5,6,4';
+
+	$GLOBALS['ISC_CFG']['AddonModules'] = '';
+
+	$GLOBALS['ISC_CFG']['AKBIsConfigured'] = '0';
+	$GLOBALS['ISC_CFG']['AKBPath'] = '';
+	$GLOBALS['ISC_CFG']['ARSPageIds'] = '';
+	$GLOBALS['ISC_CFG']['ARSIntegrated'] = '';
+
+	$GLOBALS['ISC_CFG']['ShowProductPrice'] = 1;
+	$GLOBALS['ISC_CFG']['ShowProductSKU'] = 1;
+	$GLOBALS['ISC_CFG']['ShowProductWeight'] = 1;
+	$GLOBALS['ISC_CFG']['ShowProductBrand'] = 1;
+	$GLOBALS['ISC_CFG']['ShowProductShipping'] = 1;
+	$GLOBALS['ISC_CFG']['ShowProductRating'] = 1;
+	$GLOBALS['ISC_CFG']['ProductImageMode'] = 'lightbox';
+
+	$GLOBALS['ISC_CFG']['ShowAddThisLink'] = 1;
+
+	// DO NOT CHANGE THIS VARIABLE OR YOU WILL BREAK ORDERS
+	$GLOBALS['ISC_CFG']["EncryptionToken"] = '25376e7dfcf509f2c1b7d14cb4f00cdb';
+
+	$GLOBALS['ISC_CFG']["EnableWishlist"] = 1;
+	$GLOBALS['ISC_CFG']["EnableAccountCreation"] = 1;
+	$GLOBALS['ISC_CFG']['EnableProductReviews'] = 1;
+	$GLOBALS['ISC_CFG']['EnableProductComparisons'] = 1;
+	$GLOBALS['ISC_CFG']["EnableOrderComments"] = 1;
+	$GLOBALS['ISC_CFG']["EnableOrderTermsAndConditions"] = 0;
+	$GLOBALS['ISC_CFG']["OrderTermsAndConditionsType"] = '';
+	$GLOBALS['ISC_CFG']["OrderTermsAndConditionsLink"] = '';
+	$GLOBALS['ISC_CFG']["OrderTermsAndConditions"] = '';
+
+	// Logo Settings
+	$GLOBALS['ISC_CFG']["LogoFields"] = array (
+);
+	$GLOBALS['ISC_CFG']["ForceWebsiteTitleText"] = 0;
+	$GLOBALS['ISC_CFG']['UseAlternateTitle'] = 0;
+	$GLOBALS['ISC_CFG']['AlternateTitle'] = '';
+	$GLOBALS['ISC_CFG']['UsingLogoEditor'] = 0;
+	$GLOBALS['ISC_CFG']['UsingTemplateLogo'] = 0;
+
+	$GLOBALS['ISC_CFG']['AffiliateConversionTrackingCode'] = '';
+
+	$GLOBALS['ISC_CFG']['GuestCustomerGroup'] = 0;
+	$GLOBALS['ISC_CFG']['ForwardInvoiceEmails'] = '';
+
+	// Mail Settings
+	$GLOBALS['ISC_CFG']['MailUseSMTP'] = 0;
+	$GLOBALS['ISC_CFG']['MailSMTPServer'] = '';
+	$GLOBALS['ISC_CFG']['MailSMTPUsername'] = '';
+	$GLOBALS['ISC_CFG']['MailSMTPPassword'] = '';
+	$GLOBALS['ISC_CFG']['MailSMTPPort'] = '';
+
+	// Curl Proxy Settings
+	$GLOBALS['ISC_CFG']['HTTPProxyServer'] = '';
+	$GLOBALS['ISC_CFG']['HTTPProxyPort'] = '';
+	$GLOBALS['ISC_CFG']['HTTPSSLVerifyPeer'] = 0;
+
+	// Digital Download Settings
+	$GLOBALS['ISC_CFG']['DigitalOrderHandlingFee'] = 0;
+
+	// Accounting Settings
+	$GLOBALS['ISC_CFG']['AccountingMethods'] = '';
+
+	// Live Chat Modules
+	$GLOBALS['ISC_CFG']['LiveChatModules'] = '';
+
+	//Category and Brand image dimensions
+	$GLOBALS['ISC_CFG']['CategoryPerRow'] = 3;
+	$GLOBALS['ISC_CFG']['CategoryImageWidth'] = 120;
+	$GLOBALS['ISC_CFG']['CategoryImageHeight'] = 120;
+	$GLOBALS['ISC_CFG']['CategoryDefaultImage'] = '';
+	$GLOBALS['ISC_CFG']['BrandPerRow'] = 3;
+	$GLOBALS['ISC_CFG']['BrandImageWidth'] = 120;
+	$GLOBALS['ISC_CFG']['BrandImageHeight'] = 120;
+	$GLOBALS['ISC_CFG']['BrandDefaultImage'] = '';
+
+	// Product Images
+	$GLOBALS['ISC_CFG']['DefaultProductImage'] = 'template';
+
+	//Display the 'Add to Cart' link on all the product panels
+	$GLOBALS['ISC_CFG']['ShowAddToCartLink'] = 1;
+
+	$GLOBALS['ISC_CFG']['CategoryListingMode'] = 'emptychildren';
+	$GLOBALS['ISC_CFG']['CategoryDisplayMode'] = 'grid';
+	$GLOBALS['ISC_CFG']['TagCloudMinSize'] = 80;
+	$GLOBALS['ISC_CFG']['TagCloudMaxSize'] = 300;
+
+	// Bulk Discounts
+	$GLOBALS['ISC_CFG']['BulkDiscountEnabled'] = 1;
+
+	$GLOBALS['ISC_CFG']['EnableProductTabs'] = 0;
+
+	$GLOBALS['ISC_CFG']['MultipleShippingAddresses'] = 0;
+
+	// Vendor Edition Settings
+	$GLOBALS['ISC_CFG']['VendorLogoSize'] = '120x120';
+	$GLOBALS['ISC_CFG']['VendorPhotoSize'] = '120x120';
+
+	// The factoring dimension for a shipping quote (depth, height or width with default of depth)
+	$GLOBALS['ISC_CFG']['ShippingFactoringDimension'] = 'depth';
+
+	// Array of the getting started steps that have been completed
+	$GLOBALS['ISC_CFG']['GettingStartedCompleted'] = array (
+  0 => 'settings',
+  1 => 'design',
+  2 => 'shippingOptions',
+  3 => 'paymentMethods',
+);
+
+	// The favicon file
+	$GLOBALS['ISC_CFG']['Favicon'] = 'favicon.ico';
+
+	// Session settings
+	$GLOBALS['ISC_CFG']['SessionSavePath'] = '';
+
+	// Optimizer Settings
+	$GLOBALS['ISC_CFG']['OptimizerMethods'] = array (
+);
+
+	// Advance Search format (search all)
+	$GLOBALS['ISC_CFG']['SearchDefaultProductSort'] = 'relevance';
+	$GLOBALS['ISC_CFG']['SearchDefaultContentSort'] = 'relevance';
+	$GLOBALS['ISC_CFG']['SearchProductDisplayMode'] = 'list';
+	$GLOBALS['ISC_CFG']['SearchResultsPerPage'] = 16;
+	$GLOBALS['ISC_CFG']['SearchOptimisation'] = 'fulltext';
+
+	// Abandon Orders
+	$GLOBALS['ISC_CFG']['AbandonOrderLifetime'] = '30';
+
+	// Product Image Settings
+	$GLOBALS['ISC_CFG']['ProductImagesStorewideThumbnail_width'] = 120;
+	$GLOBALS['ISC_CFG']['ProductImagesStorewideThumbnail_height'] = 120;
+	$GLOBALS['ISC_CFG']['ProductImagesProductPageImage_width'] = 220;
+	$GLOBALS['ISC_CFG']['ProductImagesProductPageImage_height'] = 220;
+	$GLOBALS['ISC_CFG']['ProductImagesGalleryThumbnail_width'] = 30;
+	$GLOBALS['ISC_CFG']['ProductImagesGalleryThumbnail_height'] = 30;
+	$GLOBALS['ISC_CFG']['ProductImagesZoomImage_width'] = 1280;
+	$GLOBALS['ISC_CFG']['ProductImagesZoomImage_height'] = 1280;
+	$GLOBALS['ISC_CFG']['ProductImagesTinyThumbnailsEnabled'] = 1;
+	$GLOBALS['ISC_CFG']['ProductImagesImageZoomEnabled'] = 1;
+
+	// Variable used to force browsers to re-download already cached
+	// stylesheets/Javascript. Set to a random value during the upgrade.
+	$GLOBALS['ISC_CFG']['JSCacheToken'] = 1;
---
+++ gerenciar/index.php
@@ -0,0 +1,21 @@
+<head>
+  <title>PAINEL DE CONTROLE</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+	<meta name="robots" content="noindex, nofollow" />
+    <link rel="SHORTCUT ICON" href="../product_images/favicon.ico" />
+	<style type="text/css" media="screen">
+    body {padding:0px;margin:0px;}
+    </style>
+</head>
+<body>
+<?php
+include "../config/config.php";
+$urlloja = $GLOBALS['ISC_CFG']["ShopPath"];
+?>
+
+  <div id="container">
+  <iframe src="<?php echo $urlloja;?>/admin/index.php" width="100%" frameborder="0" height="100%" align="center" scrolling="yes"></iframe>
+  </div>
+
+</body>
+</html>
